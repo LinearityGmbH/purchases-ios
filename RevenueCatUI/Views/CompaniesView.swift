1ee849a5bdf34386ddfd25f267d88fbec692a5f6
Max Stobetskyi <myeyesareblind@gmail.com>
2024-05-16 13:26:30 +0200

    LIN-1967: DRY func

--- RevenueCatUI/Views/CompaniesView.swift
+++ RevenueCatUI/Views/CompaniesView.swift
@@ -10,28 +10,44 @@ import SwiftUI
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 struct CompanyLogosView: View {
-    private func colorise(_ image: Image) -> some View {
-        image
+    @ViewBuilder
+    private func logoIcon(_ resource: ImageResource, spacer: Bool) -> some View {
+        Image(resource)
             .renderingMode(.template)
             .foregroundStyle(.primary)
+        if spacer {
+            Spacer()
+        }
     }
     
     var body: some View {
-        HStack {
-            colorise(Image(.iconAmazon))
-            Spacer()
-            colorise(Image(.iconApple))
-            Spacer()
-            colorise(Image(.iconUniversalPictures))
-            Spacer()
-            colorise(Image(.iconMcdonalds))
-            Spacer()
-            colorise(Image(.iconNasa))
-            Spacer()
-            colorise(Image(.iconSpacex))
-            Spacer()
-            colorise(Image(.iconGoogle))
+        let items = Array([
+            ImageResource.iconAmazon,
+            ImageResource.iconApple,
+            ImageResource.iconUniversalPictures,
+            ImageResource.iconMcdonalds,
+            ImageResource.iconNasa,
+            ImageResource.iconSpacex,
+            ImageResource.iconGoogle
+        ]
+            .enumerated()
+        )
+        return HStack {
+            ForEach(items, id: \.offset) { (i, resource) in
+                logoIcon(resource, spacer: i != items.count - 1)
+            }
         }
         .frame(height: 18)
     }
 }
+
+#if DEBUG
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+@available(macOS, unavailable)
+@available(tvOS, unavailable)
+#Preview {
+    CompanyLogosView()
+}
+
+#endif
